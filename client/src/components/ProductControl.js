065d08a85de227ad9633a0ed33de45b6c79669b7
ChepkemoiPeris <pmemoi2015@gmail.com>
2024-11-10 20:25:10 +0300

    update configmaps

--- client/src/components/ProductControl.js
+++ client/src/components/ProductControl.js
@@ -18,7 +18,8 @@ class ProductControl extends Component {
             uploadPhoto: null
             
         };
-        this.apiUrl = process.env.REACT_APP_BACKEND_API_URL || 'http://localhost:5000';
+        this.apiUrl = process.env.REACT_APP_BACKEND_API_URL || 'http://localhost:5000'; 
+        
     }
     
     componentDidMount(){
